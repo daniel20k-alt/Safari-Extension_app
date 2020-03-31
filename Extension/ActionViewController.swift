789bd51db57fc5b85161b3b30afc7b532697a8a9
Testing_author <testing_beta@mail.com>
2020-03-31 23:08:37 +0300

    Configuring the interface builder.

--- Extension/ActionViewController.swift
+++ Extension/ActionViewController.swift
@@ -10,8 +10,7 @@ import UIKit
 import MobileCoreServices
 
 class ActionViewController: UIViewController {
-
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet var script: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,14 +19,15 @@ class ActionViewController: UIViewController {
             if let itemProvider = inputItem.attachments?.first {
                 itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [weak self] (dict, error) in
                     
-                    
-                    //work to do here
+                    guard let itemDictionary = dict as? NSDictionary else { return }
+                    guard let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary else { return }
+                    print(javaScriptValues)
+                    }
                     
                 }
             }
         }
     
-    }
 
     @IBAction func done() {
         // Return any edited content to the host app.
